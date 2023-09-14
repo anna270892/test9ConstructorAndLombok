dab24ca2a862e52365d65fd532bfc4ba20c446eb
Anna Kochergina <akochergina92@gmail.com>
2023-09-14 21:49:48 +0300

    Lombok

--- src/main/java/Conditioner.java
+++ src/main/java/Conditioner.java
@@ -1,50 +1,16 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Conditioner {
-    private int maxTemperature = 30; //максимальная температура
-    private int minTemperature = 10; //минимальная температура
-    private int currentTemperature = minTemperature; //поле с минимальной текущей температурой, присвоили чтоб ноль не инициализировался
-    public int getCurrentTemperature() {  //возвращает текущую температуру
-        return currentTemperature;
-    }
+    private int id;
+    private String name;
+    private int maxTemperature ;
+    private int minTemperature ;
+    private int currentTemperature ;
 
-    public int getMaxTemperature() { //возвращает максимальную температуру
-        return maxTemperature;
-    }
-
-    public int getMinTemperature() { //возвращает минимальную температуру
-        return minTemperature;
-    }
-
-    //конструктор, который возьмет данные в указанных параемтрах minTemperature и maxTemperature
-    public Conditioner(int minTemperature, int maxTemperature) {
-        this.minTemperature = minTemperature; //поле = параметр
-        this.maxTemperature = maxTemperature; //поле = параметр
-        this.currentTemperature = minTemperature; //поле = параметр
-
-    }
-
-    public Conditioner (int size) {
-        maxTemperature = minTemperature + size;
-    }
-
-
-    public void setCurrentTemperature(int newCurrentTemperature) {
-        if (newCurrentTemperature < minTemperature) { //если температура меньше нуля то мы не можем ее поставить
-            return;
-        }
-        if (newCurrentTemperature > maxTemperature) { //новая температура больше максиамальной уходим из метода
-            return;
-        }
-        currentTemperature = newCurrentTemperature;
-    }
-
-
-    public void increaseTemp50p() {
-        int target = currentTemperature * 3 / 2; //currentTemperature - текущая температура
-        setCurrentTemperature(target); //вызываем готовый сетер, для проверки превышения максимально допустимого значения
-    }
-
-    public void setToMaxTemperature() {    //функция не будет ни чег возвращать, кондиционер просто меняет внутри температуру и не сообщает нам ни чего
-//кондиционер знает какая у него текущая температруа и какая максимальная
-        currentTemperature = maxTemperature;
-    }
 }
--- src/test/java/ConditionerTest.java
+++ src/test/java/ConditionerTest.java
@@ -1,29 +1,9 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-//создадим кондиционер
-public class ConditionerTest {
-
+class ConditionerTest {
     @Test
-    public void test() {
-        Conditioner cond = new Conditioner(-5, 5); { //передаем новые значения параметров указанных в конструкторе
+    public void shouldCreate() {
+        Conditioner conditioner = new Conditioner();
 
-            Assertions.assertEquals(-5, cond.getMinTemperature());
-            Assertions.assertEquals(5, cond.getMaxTemperature());
-            Assertions.assertEquals(-5, cond.getCurrentTemperature()); //сравним минимальную с текущей минимальной температоурой
-        }
     }
-
-    @Test
-    public void test2() {
-        Conditioner cond = new Conditioner(10); { //передаем новые значения параметров указанных в конструкторе
-
-            Assertions.assertEquals(10, cond.getMinTemperature());
-            Assertions.assertEquals(20, cond.getMaxTemperature());
-            Assertions.assertEquals(10, cond.getCurrentTemperature()); //сравним минимальную с текущей минимальной температоурой
-        }
-    }
-
-
-
 }
